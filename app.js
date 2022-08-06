d12327db14a4e61e462adf32650e67870c56387b
Vineshgithub123 <101579229+Vineshgithub123@users.noreply.github.com>
2022-08-07 02:19:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const dotenv = require("dotenv");
 const app = new express();
 const port = 4000;
 
-dotenv.config();
+dotenv.config({ path: '.env' })
 app.use(cors());
 app.use(express.static('./dist/frontend'))
 app.use(express.json({limit: '50mb'}));
