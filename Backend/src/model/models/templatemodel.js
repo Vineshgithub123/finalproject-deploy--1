fbe1ba84c334fac2a2aac275329687c4c069e328
Vineshgithub123 <101579229+Vineshgithub123@users.noreply.github.com>
2022-08-07 01:52:07 +0530

    Delete Backend directory

--- Backend/src/model/models/templatemodel.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose');
-const Schema=mongoose.Schema;
-const tempSchema=new Schema({
-    tempid: [],
-    userid: String
-});
-
-var TempSchema=mongoose.model('tempDatas',tempSchema);
-module.exports=TempSchema;
